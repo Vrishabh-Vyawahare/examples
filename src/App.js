dc8fa16604781a7316cc5a0e07a351d4300ab85d
vrishabh vyawahare <vrishabh22vyawahare@gmail.com>
2024-08-31 13:46:22 +0530

    version1 added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@
 function App() {
   return (
     <div>
-  
-          <h1>Welcome to new Features branch</h1>
-    
-    
+          <h1>Git Project Demo</h1>
+          <h3>My first commit</h3>
+          <h5>My second commit</h5>
+          <h3>This is a version1 branch</h3>
     </div>
   );
 }
